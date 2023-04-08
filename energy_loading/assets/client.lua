0524a18ed5888df27677cb6b2192f867457b1a36
Kipant <130110016+Kipant@users.noreply.github.com>
2023-04-08 02:21:08 +0200

    Add files via upload

---
+++ energy_loading/assets/client.lua
@@ -0,0 +1,1 @@
+print("discord.gg/33leaks")
---
+++ energy_loading/assets/scripts/config.js
@@ -0,0 +1,18 @@
+Config = {}; // Don't touch
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", icon: "assets/media/icons/discord.png", link: "https://discord.gg/4SVbEcvS"},
+
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Team list
+Config.Team = [
+    {name: "Mustafa Efendi", description: "Vlasnik", color: "#fff", image: "https://cdn.discordapp.com/attachments/1037511117754343465/1094047891347222538/transparent-logo.png"},
+    {name: "Huso", description: "Vlasnik", color: "#fff", image: "https://cdn.discordapp.com/attachments/1037511117754343465/1094047891347222538/transparent-logo.png"},
+    {name: "Muki HD", description: "Skripter", color: "#fff", image: "https://cdn.discordapp.com/attachments/1037511117754343465/1094047891347222538/transparent-logo.png"},
+
+];
